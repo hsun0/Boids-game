fe479dfca123650fefed1a12df5d99d768053441
hsun0 <ceng45428@gmail.com>
2024-10-22 15:35:40 +0800

    Add food

--- main/main.py
+++ main/main.py
@@ -3,6 +3,7 @@ import source as src
 from bird import Bird
 import random
 import math
+from food import Food
 
 def main()->None:
     # 要有這個不然無法使用 pygame 的功能
@@ -20,6 +21,13 @@ def main()->None:
         angle = random.uniform(-math.pi, math.pi)
         birds.append(Bird(x, y, angle))
 
+    # 初始化食物
+    foods = []
+    for _ in range(src.foodNum):
+        x = random.uniform(0, src.windowSize[0])
+        y = random.uniform(0, src.windowSize[1])
+        foods.append(Food(x, y))
+
     # Main Loop
     running = True
     clock = pygame.time.Clock()
@@ -30,21 +38,33 @@ def main()->None:
         
         # Note: pygame 會將所有的東西畫在緩衝區，然後再透過 flip() 顯示到螢幕上
         # 清除畫面
-        window.fill(src.Colors['black'])
+        window.fill(src.Colors['white'])
+
+        removeList = []
 
         # 畫出所有的鳥
         for bird in birds:
             # 更新鳥的座標
             bird.move(birds)
-            point = [
-                (bird.x + src.birdSize * 2 * math.cos(bird.angle), bird.y + src.birdSize * 2 * math.sin(bird.angle)),
-                (bird.x + src.birdSize * math.cos(bird.angle + 2 * math.pi / 3), bird.y + src.birdSize * math.sin(bird.angle + 2 * math.pi / 3)),
-                (bird.x + src.birdSize * math.cos(bird.angle - 2 * math.pi / 3), bird.y + src.birdSize * math.sin(bird.angle - 2 * math.pi / 3))
-            ]
-            pygame.draw.polygon(window, src.Colors['white'], point)
+            bird.eat(foods)
+            bird.display(window)
+            if bird.energy <= 0:
+                removeList.append(bird)
+
+        for food in foods:
+            food.display(window)
+
+        # 移除能量為 0 的鳥
+        for bird in removeList:
+            birds.remove(bird)
+
+        # 每十秒減少一次所有鳥的能量
+        if pygame.time.get_ticks() % 5000 < clock.get_time():
+            for bird in birds:
+                bird.energy -= 1
 
         # 將緩衝區顯示到螢幕上
-        pygame.display.flip() 
+        pygame.display.flip()
 
         # FPS
         clock.tick(src.FPS)
